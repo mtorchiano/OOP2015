0f753307e08869055d1f981fa20de2cff98a133c
mtorchiano <marco.torchiano@polito.it>
2015-03-05 11:31:13 +0100

    Initial version

---
+++ Sample/src/Main.java
@@ -0,0 +1,16 @@
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello " + getName());
+
+	}
+
+	private static String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
